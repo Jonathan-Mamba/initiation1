ab2c8e0818e4444b179d894b2fecd8b44ae161d6
Jonathan-Mamba <jojodiallo.jd@gmail.com>
2024-09-28 09:36:32 +0400

    get_term working like expected

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import math
 import icecream
 import typing
-from trees import Operation
+from trees import Operation, Term, Component
 import funcs
 
 
-def get_term(operation: str, operators: typing.Iterable, start_index: int = 0) -> str:
+def get_term(operation: str, operators: typing.Iterable,  start_index: int = 0) -> tuple[str]:
     returned_str: str = ""
     found_operators: bool = False
+    print("\nNew call ")
     for i in range(start_index, len(operation)):
+        icecream.ic(returned_str, i)
         char = operation[i]
-        if char == " ":
+        if char in (" ", "(", ")"):
             continue
 
         if char == "-" and returned_str == "":
@@ -26,80 +28,64 @@ def get_term(operation: str, operators: typing.Iterable, start_index: int = 0) -
         if char.isdigit():
             returned_str += char
 
-        if char in operators and not found_operators and i > 0:
-            return (returned_str, start_index, char)
+        if char in operators and not found_operators and i > start_index:
+            return (returned_str, str(i), char)
         
-    return (returned_str, start_index, "")
+    return (returned_str, str(i), "")
 
-def get_operation(operation: str, operators: typing.Iterable) -> Operation:
-    index = -1
-    first_term = ""
-    operator = ""
-    second_term = ""
 
-    for char in operation:
-        index += 1
-        icecream.ic(char, index)
-        if char == " ":
-            continue
+def get_operation(operation: str, operators: typing.Iterable, priorities: dict[str, int], parenthesis: bool = False) -> Operation:
+    index = 0
+    terms: list[str] = []
+    operations: dict[int, list[Operation]] = dict.fromkeys(range(max(priorities.values())))
 
-        if char == '-' and first_term == "":
-            first_term = "-"
+    _i = 0
+    icecream.ic(operation)
+    while True:
+        term = get_term(operation, operators, index)
+        if len(terms) > 0 and terms[-1] == term[0][0] == "-":# si ya un - opérateur puis un pour négatif
+            term = (term[0][1::], term[1], term[2]) # on enlèle celui pour le négatif
+        terms.append(term[0])
 
-        if char == ".":
-            if "." not in first_term:
-                first_term += "."
-            else:
-                raise ValueError("mais chef (deux points)")
-        
-        if char.isdigit():
-            first_term += char
-
-        if char in operators:
-            operator = char
+        index = int(term[1])
+        icecream.ic(terms, index)
+        if term[2] == "":
             break
+        else:
+            terms.append(term[2])
+        _i += 1
 
-
-    for i in range(index + 1, len(operation)):
-        char = operation[i]
-        icecream.ic(char, i)
-        if char == " ":
+    for i, term in enumerate(terms):
+        if term is None:
             continue
 
-        if char == '-' and second_term == "":
-            second_term = "-"
-
-        if char == ".":
-            if "." not in second_term:
-                second_term += "."
-            else:
-                raise ValueError("mais chef (deux points)")
+        if term in operators:
+            if 0 < i < len(terms) - 1:
+                if operations[priorities[term]] is None: 
+                    operations[priorities[term]] = []
+                
+                operations[priorities[term]] = Operation(float(terms[i - 1]), term, float(terms[i + 1]))
+                terms[i - 1], terms[i], terms[i + 1] = None, None, None #on sait jamais
         
-        if char.isdigit():
-            second_term += char
-
-        if char in operators:
-            raise ValueError("chef tu fous quoi (trop d'opérateurs ptet)")
-
-    try:
-        float(first_term)
-    except ValueError:
-        print(">> manque le premier chiffre")
-        quit(1)
+        else:
+            try: float(term)
+            except ValueError:
+                print(f">> {term} n'est pas un chiffre")
+                quit(-1)
     
-    try:
-        float(second_term)
-    except ValueError:
-        print(">> manque le 2e chiffre")
-        quit(1)
+    icecream.ic(operations)
+    for i in range(max(priorities.values()), 0, -1):
+        
+        for j in range(1, len(operations[i])):
+            operations[i][j]
 
     
-    return Operation(float(first_term), operator, float(second_term))
+    return Operation(Term(0), "+", Term(0))
 
         
 
 def main():
-    OPERATORS = {
+    OPERATORS: dict[str, typing.Callable] = {
         "+": funcs.add,
         "-": funcs.sub,
         "*": funcs.mult,
@@ -108,12 +94,22 @@ def main():
         ":": funcs.fdiv,
         "%": funcs.mod
     }
+
+    PRIORITIES: dict[str, int] = {
+        "+": 0,
+        "-": 0,
+        "*": 1,
+        "^": 2,
+        "/": 1,
+        ":": 1,
+        "%": 1
+    }
     
     while True:
-        operation: Operation = get_operation(input(">> "), OPERATORS.keys())
+        operation: Operation = get_operation(input(">> "), OPERATORS.keys(), PRIORITIES)
         result = operation.get_result(OPERATORS)
         print(f"> {result}")
-        if "n" in input(">> quit <y/n> ? "):
+        if "y" in input(">> quit <y/n> ? "):
             break
 
 
--- trees.py
+++ trees.py
@@ -3,22 +3,25 @@ from typing import Callable, Any
 import math
 
 
-class Component(abc.ABC):
+class Component(abc.ABC):    
     @abc.abstractmethod
-    def get_result(self, operators: dict[str, Callable[list[float, float], float]]) -> float:
-        return operators.get(self.operator)(self.first_term, self.second_term)
+    def get_result(self, operators: dict[str, Callable[list[float, float], float]]) -> float:# polymorphism go crazyyyy
+        return operators.get(self.operator)(self.first_term.get_result(operators), self.second_term.get_result(operators))
     
 
 class Term(Component):
     def __init__(self, value: float = 0.0) -> None:
         self.value = value
     
-    def get_result(self, operators: dict[str, Callable[[], float]]) -> float:
+    def get_result(self, _) -> float: #on s'en fout du 2e argument
         return self.value
+    
+    def __str__(self) -> str:
+        return str(self.value)
 
 
 class Operation(Component):
-    def __init__(self, first: float, operator: str, second: float, parenthesis: bool = False) -> None:
+    def __init__(self, first: Component, operator: str, second: Component, parenthesis: bool = False) -> None:
         self.first_term = first
         self.operator = operator
         self.second_term = second
