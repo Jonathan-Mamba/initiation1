f04efc0498ef7e84699495c67765a71a737cc1aa
Jonathan-Mamba <jojodiallo.jd@gmail.com>
2024-09-29 21:57:10 +0400

    v1.5 @pointer debugged --log

--- trees.py
+++ trees.py
@@ -3,20 +3,20 @@ from typing import Callable, Any
 import math
 
 #c un binary tree
-class Component(abc.ABC):    
+class ExpressionPart(abc.ABC):    
     @abc.abstractmethod
     def get_result(self, operators: dict[str, Callable[list[float, float], float]]) -> float:# polymorphism go crazyyyy
         pass
-    
+
     @abc.abstractmethod
-    def hasnan(self) -> bool:
+    def set_parenthesis(self, b: bool):
         pass
 
     def __repr__(self) -> str:
         return self.__str__()
     
 
-class Term(Component):# classe Leaf
+class Term(ExpressionPart):# classe Leaf
     def __init__(self, value: float = 0.0) -> None:
         self.value = value
     
@@ -26,22 +26,26 @@ class Term(Component):# classe Leaf
     def get_result(self, _) -> float: #on s'en fout du 2e argument
         return self.value
     
-    def hasnan(self) -> bool:
-        return math.isnan(self.value)
+    def set_parenthesis(self, b: bool):
+        pass
 
 
-class Operation(Component):# classe Tree
-    def __init__(self, first: Component, operator: str, second: Component, parenthesis: bool = False) -> None:
+class ExpressionTree(ExpressionPart):# classe Tree
+    def __init__(self, first: ExpressionPart, operator: str, second: ExpressionPart, parenthesis: bool = False) -> None:
         self.first_term = first
         self.operator = operator
         self.second_term = second
         self.parenthesis = parenthesis
 
     def __str__(self) -> str:
-        return f"{'(' if self.parenthesis else ''}{self.first_term} {self.operator} {self.second_term}{')' if self.parenthesis else ''}" 
-    
+        if self.parenthesis:
+            return f"({self.first_term} {self.operator} {self.second_term})" 
+        return f"{self.first_term} {self.operator} {self.second_term}" 
+
     def get_result(self, operators: dict[str, Callable[list[float, float], float]]) -> float:
         return operators.get(self.operator)(self.first_term.get_result(operators), self.second_term.get_result(operators))
     
-    def hasnan(self) -> bool:
-        return self.first_term.hasnan() or self.second_term.hasnan() 
+    def set_parenthesis(self, b: bool):
+        self.parenthesis = b
+        self.first_term.set_parenthesis(b)
+        self.second_term.set_parenthesis(b)
